e2248ace9e6e96925639502723aa7bf917853ef6
Ahmad Zain <84906344+Zain-Ahma12@users.noreply.github.com>
2021-06-05 09:01:23 +0530

    Add files via upload

---
+++ guess_the_number.py
@@ -0,0 +1,21 @@
+guess_number=50
+Chance=10
+print("Welcome to 'Guess the Number' game...")
+while Chance>0:
+    print(f"*************************Change Left : {Chance}")
+    a=int(input("Enter your number : "))
+    if (a-guess_number>10): 
+        print("Your guess is 'TOO HIGH'")
+    elif guess_number-a>10:
+        print("Your guess is 'TOO LOW'")
+    elif a==guess_number:
+        print("Your guess is 'CORRECT'*****YOU WIN*****")
+        break
+    else:
+        print("Your are really close, keep trying")
+    Chance-=1
+
+
+
+
+
